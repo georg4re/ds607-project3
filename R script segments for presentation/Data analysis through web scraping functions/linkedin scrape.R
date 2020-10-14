1539b629ee2c4b9a2450e0acd1206855488fce86
JackJosephWright <70408833+JackJosephWright@users.noreply.github.com>
2020-10-14 13:37:50 -0400

    reorganizing based on Ricks outline

---
+++ R script segments for presentation/Data analysis through web scraping functions/linkedin scrape.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+
+#load linkedin scraping functions
+source("LINK FOR LINKEDIN_SCRAPER_FUNCTIONS")
+
+#log rvest into linkedin
+library(rvest) 
+
+#Address of the login webpage
+login<-"https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin"
+
+#create a web session with the desired login address
+pgsession<-html_session(login)
+pgform<-html_form(pgsession)[[1]]  #in this case the submit is the 2nd form
+filled_form<-set_values(pgform, session_key="xxxemail_or_login_name", session_password="xxxpasswordxxx")
+submit_form(pgsession, filled_form)
+
+
+
+#scrape linkedin
+df_linkedIn<-linkedIn_scrape(link_base_url = base_url,max=max)
